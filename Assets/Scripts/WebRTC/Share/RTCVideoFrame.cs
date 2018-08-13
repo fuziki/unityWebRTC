859d3a57cbe28891f89468ae2bda20272fd66d90
fuziki <hymne.shannon.projects@gmail.com>
2018-08-13 15:50:56 +0900

    add rtcvideoframe(only class

---
+++ Assets/Scripts/WebRTC/Share/RTCVideoFrame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTCVideoFrame {
+
+    public Texture2D texture2D;
+    public long timestamp_us;
+    public RTCVideoFrame(Texture2D tex, long  ts_us)
+    {
+        texture2D = tex;
+        timestamp_us = ts_us;
+    }
+    public void setTexture2DbyRenderTexture(RenderTexture rtex)
+    {
+        if (texture2D == null) texture2D = new Texture2D((int)rtex.width, (int)rtex.height, TextureFormat.ARGB32, false);
+        RenderTexture currentRT = RenderTexture.active;
+        RenderTexture.active = rtex;
+        if (texture2D.width != rtex.width || texture2D.height != rtex.height)
+        {
+            texture2D = new Texture2D((int)rtex.width, (int)rtex.height, TextureFormat.ARGB32, false);
+        }
+        texture2D.ReadPixels(new Rect(0, 0, rtex.width, rtex.height), 0, 0);
+        texture2D.Apply();
+        RenderTexture.active = currentRT;
+    }
+
+}
